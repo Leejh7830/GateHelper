acf747b5fe9b36ed2f87699c1b2431b8038a7f06
leejh7830 <leejh7830@naver.com>
2025-08-22 16:24:17 -0400

    250822 16:30 변경사항 없음

--- Util.cs
+++ Util.cs
@@ -40,7 +40,7 @@ namespace GateHelper
             {
                 try
                 {
-                    string initialContent = "v1.0.0 (2025.04.15)\r\n- Initial Release";
+                    string initialContent = "v1.0.0 (2024.03.03)\r\n- Initial Release";
                     File.WriteAllText(releaseNotesPath, initialContent);
                 }
                 catch (Exception ex)
